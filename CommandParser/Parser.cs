81a8c6e5a6d113a89999a46de2890336b47e594b
Daniel Destouche <daniel@Daniels-MacBook-Air.local>
2017-09-03 12:13:27 -0400

    Update

--- CommandParser/Parser.cs
+++ CommandParser/Parser.cs
@@ -7,12 +7,15 @@ namespace DAT.CommandParser
 {
     public static class Parser
     {
-        public static void ParseArguments(Command command, string[] args, TextWriter outputWriter) {
+        public static void ParseArguments(Command command, string[] args, TextWriter outputWriter)
+        {
             if (command == null)
                 throw new ArgumentNullException(nameof(command));
 
             if (outputWriter == null)
                 throw new ArgumentNullException(nameof(outputWriter));
+
+            
         }
     }
 }
